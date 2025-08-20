effb0fcb252f01dda556297f28e022b76abdb135
Artur S. Farias <artur06farias@gmail.com>
2025-08-20 11:45:49 -0300

    HashRouter

--- src/routes/PageManager.tsx
+++ src/routes/PageManager.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Menu from "../components/Menu";
 import Profile from "../pages/Profile";
 import Contact from "../pages/Contact";
